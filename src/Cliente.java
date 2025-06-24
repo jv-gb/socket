324bfa9b6dbacd934b843a2584db2ac86428f82b
Your Name <joaovgbv@gmail.com>
2025-06-24 13:45:14 -0300

    cliente TCP criado

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,29 @@
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Cliente {
     public static void main(String[] args) {
+    String ip = "127.0.0.1";
+    int port = 12345;
+    Socket socket = null;
+
+    try{
+        socket = new Socket(ip, port);
+
+        // Cria writer para enviar dados ao servidor
+        PrintWriter out = new PrintWriter(
+                new OutputStreamWriter(socket.getOutputStream()), true
+        );
+
+        String mensagem = "Olá, servidor!";
+        out.println(mensagem);
+        System.out.println("Mensagem enviada ao servidor: " + mensagem);
+    }
+    catch(Exception e){
+        System.out.println("Erro: " + e);
+    }
+
 
     }
 }
