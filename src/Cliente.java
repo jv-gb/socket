ba9d33c2394356b0e8e47d74886ca8dfdc51fbd2
Your Name <joaovgbv@gmail.com>
2025-06-24 13:51:07 -0300

    mensagem sair

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,28 +1,39 @@
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
-    String ip = "127.0.0.1";
-    int port = 12345;
-    Socket socket = null;
+        String ip = "127.0.0.1";
+        int port = 12345;
+        Socket socket = null;
 
-    try{
-        socket = new Socket(ip, port);
+        try {
+            while (true) {
+                socket = new Socket(ip, port);
+                Scanner input = new Scanner(System.in);
+                String mensagem = input.next();
 
-        // Cria writer para enviar dados ao servidor
-        PrintWriter out = new PrintWriter(
-                new OutputStreamWriter(socket.getOutputStream()), true
-        );
 
-        String mensagem = "Olá, servidor!";
-        out.println(mensagem);
-        System.out.println("Mensagem enviada ao servidor: " + mensagem);
-    }
-    catch(Exception e){
-        System.out.println("Erro: " + e);
-    }
+                // Cria writer para enviar dados ao servidor
+                PrintWriter out = new PrintWriter(
+                        new OutputStreamWriter(socket.getOutputStream()), true
+                );
+
+
+
+                out.println(mensagem);
+                System.out.println("Mensagem enviada ao servidor: " + mensagem);
+
+                if(mensagem.equals("sair")){
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+        }
 
 
     }
